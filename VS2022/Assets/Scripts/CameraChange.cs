71c2bc5fd0a777112fd4cc0e29e32f138bd3c6c0
Martovvi <55866297+Martovvi@users.noreply.github.com>
2022-07-02 20:26:11 +0200

    Added functional prev and nextbutton with camera transitions

--- VS2022/Assets/Scripts/CameraChange.cs
+++ VS2022/Assets/Scripts/CameraChange.cs
@@ -2,22 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraChange : MonoBehaviour
 {
-    public CinemachineVirtualCamera[] cameras;
-    public int currentCamera = 0;
-    
-    public void NextCamera()
+    public GameObject[] cameras;
+    public int currentIndex = 0;
+    [SerializeField] private GameObject prevButton;
+    [SerializeField] private GameObject nextButton;
+
+    void Awake()
     {
-        for (int i = 0; i < cameras.Length; i++)
+        if (cameras.Length > 0)
         {
-            cameras[i].GetComponent<CinemachineVirtualCamera>().enabled = false;
+            for (int i = 1; i < cameras.Length; ++i)
+            {
+                cameras[i].SetActive(false);
+            }
         }
-        if (currentCamera < cameras.Length)
-        { 
-            cameras [currentCamera].GetComponent<CinemachineVirtualCamera> ().enabled = true;
-            currentCamera++;
+        prevButton.SetActive(false);
+    }
+    
+    public int CurrentIndex
+    {
+        get
+        {
+            return currentIndex;
+        }
+        set
+        {
+            if (cameras[currentIndex] != null)
+            {
+                //set the current active object to inactive, before replacing it
+                GameObject activeObj = cameras[currentIndex];
+                activeObj.SetActive(false);
+            }
+
+            if (value < 0)
+            {
+                currentIndex = 0;
+            }
+            
+            else if (value > cameras.Length - 1)
+            {
+                currentIndex = cameras.Length -1;
+            }
+            else
+            {
+                currentIndex = value;
+            }
+            if (cameras[currentIndex] != null)
+            {
+                GameObject activeObj = cameras[currentIndex];
+                activeObj.SetActive(true);
+            }
+
+            if (currentIndex == 0)
+            {
+                prevButton.SetActive(false);
+            }
+            else if (currentIndex == cameras.Length -1)
+            {
+                nextButton.SetActive(false);
+            }
+            else
+            {
+                prevButton.SetActive(true);
+                nextButton.SetActive(true);
+            }
+            
+            
         }
     }
+    
+    public void Next(int direction)
+    {
+        if (direction == 0)
+            CurrentIndex--;
+        else
+            CurrentIndex++;
+    }
+    
 }
