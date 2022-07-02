cef1c7801be38c9176e59b87ce5d24abcbe75998
kigari94 <36a8d9lf21x>
2022-07-02 16:00:24 +0200

    adding demonstrationManager

---
+++ VS2022/Assets/Scripts/Demonstration.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine.Events;
+
+[Serializable]
+    public class Demonstration
+    {
+        public string Instruction;
+        public UnityEvent OnStart;
+        public UnityEvent OnEnd;
+    }
+
